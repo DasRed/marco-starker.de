bcd7eb1973ade449ba1861d42f1fb7c337f61758
Marco Starker <marco.starker@gmx.net>
2024-10-05 15:20:51 +0200

    update

--- webpack.config.js
+++ webpack.config.js
@@ -8,58 +8,54 @@ import {fileURLToPath} from 'url';
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 export default {
-  mode: "development",
-  entry: "./src/assets/js/app.js",
-  output: {
-    filename: "assets/js/app.js",
-    path: path.resolve(__dirname, "dist"),
-  },
-  devServer: {
-    static: path.resolve(__dirname, "dist"),
-    port: 8080,
-    hot: true,
-    watchFiles: ["src/**/*.html"],
-  },
-  plugins: [
-    new HtmlWebpackPlugin({ template: "./src/index.html", filename: "./index.html", minify: false }),
-    new miniCssExtractPlugin({
-      filename: "assets/css/index.css",
-    }),
-    new CopyPlugin({
-      patterns: [{ from: "src/assets/images", to: "assets/images" }],
-    }),
-  ],
-  module: {
-    rules: [
-      {
-        test: /\.(scss)$/,
-        use: [
-          {
-            // Extracts CSS for each JS file that includes CSS
-            loader: miniCssExtractPlugin.loader,
-          },
-          {
-            // Interprets `@import` and `url()` like `import/require()` and will resolve them
-            loader: "css-loader",
-          },
-          {
-            // Loader for webpack to process CSS with PostCSS
-            loader: "postcss-loader",
-            options: {
-              postcssOptions: {
-                plugins: [autoprefixer],
-              },
-            },
-          },
-          {
-            // Loads a SASS/SCSS file and compiles it to CSS
-            loader: "sass-loader",
-          },
-        ],
-      },
+    mode:         "development",
+    entry:        "./src/assets/js/app.js",
+    output:       {
+        filename: "assets/js/app.js",
+        path:     path.resolve(__dirname, "dist"),
+    },
+    devServer:    {
+        static:     path.resolve(__dirname, "dist"),
+        port:       8080,
+        hot:        true,
+        watchFiles: ["src/**/*.html"],
+    },
+    plugins:      [
+        new HtmlWebpackPlugin({template: "./src/index.html", filename: "./index.html", minify: false}),
+        new miniCssExtractPlugin({filename: "assets/css/index.css"}),
+        new CopyPlugin({patterns: [{from: "src/assets/images", to: "assets/images"}]}),
     ],
-  },
-  optimization: {
-    minimize: false,
-  },
+    module:       {
+        rules: [
+            {
+                test: /\.(scss)$/,
+                use:  [
+                    {
+                        // Extracts CSS for each JS file that includes CSS
+                        loader: miniCssExtractPlugin.loader,
+                    },
+                    {
+                        // Interprets `@import` and `url()` like `import/require()` and will resolve them
+                        loader: "css-loader",
+                    },
+                    {
+                        // Loader for webpack to process CSS with PostCSS
+                        loader:  "postcss-loader",
+                        options: {
+                            postcssOptions: {
+                                plugins: [autoprefixer],
+                            },
+                        },
+                    },
+                    {
+                        // Loads a SASS/SCSS file and compiles it to CSS
+                        loader: "sass-loader",
+                    },
+                ],
+            },
+        ],
+    },
+    optimization: {
+        minimize: false,
+    },
 };
